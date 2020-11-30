05c879920c8a904c61241cdd367e0c7eeaaf3bd4
lijiaqi <lijiaqi.k1t@bytedance.com>
2020-11-30 21:46:30 +0800

    change some xml

--- app/src/main/java/com/example/android_demo_application/fragment_adapters/ShouyeAdapter.kt
+++ app/src/main/java/com/example/android_demo_application/fragment_adapters/ShouyeAdapter.kt
@@ -5,20 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_demo_application.R
+import com.example.android_demo_application.fragments.ShouyeBannerFragment
 import kotlinx.android.synthetic.main.shouye_item_1.view.*
 import kotlinx.android.synthetic.main.shouye_item_2.view.*
 
 class ShouyeAdapter(private val fragmentManager: FragmentManager, private val itemList: List<ShouyeItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    inner class BannerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class BannerViewHolder(view: View) : RecyclerView.ViewHolder(view) {}
 
-    }
-
-    inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    }
+    inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {}
 
     override fun getItemViewType(position: Int) = if (position == 0) 0 else 1
 
@@ -44,7 +44,7 @@ class ShouyeAdapter(private val fragmentManager: FragmentManager, private val it
                 publishTimeText.text = itemList[position-1].publishTime
                 titleText.text = itemList[position-1].title
                 contentText.text = itemList[position-1].content
-                typeText.text = itemList[position-1].type
+                typeText.text = itemList[position-1].superChapterName
             }
         }
     }
@@ -52,4 +52,11 @@ class ShouyeAdapter(private val fragmentManager: FragmentManager, private val it
     override fun getItemCount() = itemList.size + 1
 }
 
-class ShouyeItem(val author: String, val publishTime: String, val title: String, val content: String, val type: String, val url: String)
+class ShouyeBannerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+    override fun getItem(position: Int): Fragment {
+        return ShouyeBannerFragment()
+    }
+    override fun getCount() = 2
+}
+
+class ShouyeItem(val author: String?, val publishTime: String?, val title: String?, val content: String?, val superChapterName: String?, val link: String?)
