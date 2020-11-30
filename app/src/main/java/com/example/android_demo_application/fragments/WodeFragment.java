44a6c9639757e35881a0628a366d99165016dde3
lifangzheng <lifangzheng.1998@bytedance.com>
2020-11-30 19:00:12 +0800

    by lfz view pager xiu fu

--- app/src/main/java/com/example/android_demo_application/fragments/WodeFragment.java
+++ app/src/main/java/com/example/android_demo_application/fragments/WodeFragment.java
@@ -1,36 +1,45 @@
 package com.example.android_demo_application.fragments;
 
+/*
+* author by @李方正
+* 我的 界面的fragment
+*
+* */
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import com.example.android_demo_application.R;
-import com.example.android_demo_application.fragment_adapters.WodeAdapter;
+
 
 public class WodeFragment extends Fragment {
-    RecyclerView recyclerView;
     View view;
+    TextView textView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.wode,container,false);
-        recyclerView = view.findViewById(R.id.list_item);
+        initView();
         return view;
     }
-
+    void initView(){
+        textView = view.findViewById(R.id.qudenglu);
+    }
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        WodeAdapter adapter = new WodeAdapter();
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //启动一个新的activity  去登陆或者去注册
+            }
+        });
     }
 }
