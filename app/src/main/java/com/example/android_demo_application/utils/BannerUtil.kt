157caf893511d58622de544868b1f930138a4b75
lijiaqi <lijiaqi.k1t@bytedance.com>
2020-12-01 21:34:05 +0800

    add BannerUtil

---
+++ app/src/main/java/com/example/android_demo_application/utils/BannerUtil.kt
@@ -0,0 +1,7 @@
+package com.example.android_demo_application.utils
+
+import androidx.fragment.app.Fragment
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.lang.Exception
+
--- app/src/main/java/com/example/android_demo_application/utils/BannerUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android_demo_application.utils
-
-import androidx.fragment.app.Fragment
-
-class BannerUtils {
-    fun getBannerPics(): List<Fragment> {
-        return listOf()
-    }
-}
