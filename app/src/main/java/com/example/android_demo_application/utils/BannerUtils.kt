30b12228cd94c7ab34085fcd09442731771955d4
lijiaqi <lijiaqi.k1t@bytedance.com>
2020-12-01 17:46:14 +0800

    create BannerUtils

---
+++ app/src/main/java/com/example/android_demo_application/utils/BannerUtils.kt
@@ -0,0 +1,9 @@
+package com.example.android_demo_application.utils
+
+import androidx.fragment.app.Fragment
+
+class BannerUtils {
+    fun getBannerPics(): List<Fragment> {
+        return listOf()
+    }
+}
